21bc77b45d8888154ea8769465dea410508fc00c
Girish Tharwani <111285325+girish-tharwani@users.noreply.github.com>
2026-03-30 16:34:58 +0530

    Create TransactionScreen.java to extract transaction-related methods from AccountsScreen

---
+++ src/main/java/com/sanchay/ui/accounts/TransactionScreen.java
@@ -0,0 +1,19 @@
+// TransactionScreen.java
+
+package com.sanchay.ui.accounts;
+
+public class TransactionScreen {
+    // Extracted transaction-related methods from AccountsScreen
+
+    public void viewTransaction() {
+        // Method implementation
+    }
+
+    public void createTransaction() {
+        // Method implementation
+    }
+
+    public void deleteTransaction() {
+        // Method implementation
+    }
+}
